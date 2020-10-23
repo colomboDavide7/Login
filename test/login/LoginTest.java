9068774676eb35bffc4402d606c1e33de8f45552
Davide Colombo <davide.colombo02@universitadipavia.it>
2020-10-23 20:21:29 +0200

    Added the first test of the project

---
+++ test/login/LoginTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package login;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author davidecolombo
+ */
+public class LoginTest {
+
+    @Test
+    public void nothing() {
+        System.out.println("* LoginTest: nothing()");
+    }
+    
+}
